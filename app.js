cbc9d747f02bb239587216716c12ed6e313c55ad
Garrett Yokley <garrett.t.yokley@gmail.com>
2023-08-30 17:06:18 -0400

    Added random computer choice generator
    Initialized an array of strings with the possible choices. Used
    methods of the built in Math object to randomly pick one of the
    choices. Built a function to test the randomness of these choices.

--- app.js
+++ app.js
@@ -8,6 +8,77 @@ const rock_button = document.getElementById("rock");
 const paper_button = document.getElementById("paper");
 const scissors_button = document.getElementById("scissors");
 
-rock_button.addEventListener('click', function() {
-    console.log("you clicked rock");
-})
+
+
+function getBobsChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
+}
+
+function game(userChoice) {
+    console.log("test event listener");
+    console.log(" ");
+//    getBobsChoice();
+    let bobsChoice
+}
+
+function count() {
+
+}
+
+function main() {
+
+    rock_button.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_button.addEventListener('click', function() {
+        game("paper");
+    })
+    scissors_button.addEventListener('click', function() {
+        game("scissors");
+    })
+
+
+}
+
+main();
+
+
+
+
+
+
+//      Test randomness of computer choices     //
+
+const bob_rock_span = document.getElementById('bobs-rock-count');
+const bob_paper_span = document.getElementById('bobs-paper-count');
+const bob_scissors_span = document.getElementById('bobs-scissors-count');
+
+let rCount = 0;
+let pCount = 0;
+let sCount = 0;
+
+function testRandomness() {
+    for (let i = 0; i < 100000; i++) {
+        let cpuChoice = getBobsChoice();
+        if (cpuChoice === 'rock') {
+            rCount++;
+        } else if (cpuChoice === 'paper'){
+            pCount++;
+        } else if (cpuChoice === 'scissors'){
+            sCount++;
+        }
+    }
+    bob_rock_span.textContent = rCount;
+    bob_paper_span.textContent = pCount;
+    bob_scissors_span.textContent = sCount;
+}
+
+document.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        testRandomness();
+    }
+});
+
