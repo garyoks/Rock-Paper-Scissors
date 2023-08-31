f216f5756164a353d4c9197adabd8ab7803ef0bc
Garrett Yokley <garrett.t.yokley@gmail.com>
2023-08-30 23:18:00 -0400

    Built all the functionality
    Added a game function called when one of the buttons is clicked.
    Added a determine winner function to compare the user's choice
    with a randomly generated computer choice. Built in a play again
    button to be displayed when one of the players hits 5 wins, which
    calls a reset game function.
    
    Also moved the randomness test to the footer of the page with a
    message to the user for how to use it. Because Bob is surprisingly
    good.

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const userScore = 0;
-const bobsScore = 0;
+let userScore = 0;
+let bobsScore = 0;
+
 const userScore_span = document.getElementById("user-score");
 const bobsScore_span = document.getElementById("bobs-score");
-const score_div = document.querySelector(".score");
-const result_div = document.querySelector(".result");
+
+const resultMessage_p = document.getElementById("result-message");
+const declareWinner_p = document.getElementById("declare-winner");
+
 const rock_button = document.getElementById("rock");
 const paper_button = document.getElementById("paper");
 const scissors_button = document.getElementById("scissors");
-
-
+const playAgain_button = document.getElementById("play-again");
 
 function getBobsChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -16,45 +18,115 @@ function getBobsChoice() {
     return choices[choice];
 }
 
-function game(userChoice) {
-    console.log("test event listener");
-    console.log(" ");
-//    getBobsChoice();
-    let bobsChoice
-}
+function determine_winner(userChoice) {
+        
+        let bobsChoice = getBobsChoice();
 
-function count() {
+        // a true value will represent a win for the user and false a win for Bob
 
-}
-
-function main() {
-
-    rock_button.addEventListener('click', function() {
-        game("rock");
-    })
-
-    paper_button.addEventListener('click', function() {
-        game("paper");
-    })
-    scissors_button.addEventListener('click', function() {
-        game("scissors");
-    })
-
+        if (bobsChoice === 'rock' && userChoice === 'scissors') {
+            return false;
+        } else if (bobsChoice === 'rock' && userChoice === 'paper') {
+            return true;
+        } else if (bobsChoice === 'paper' && userChoice === 'rock') {
+            return false;
+        } else if (bobsChoice === 'paper' && userChoice === 'scissors') {
+            return true;
+        } else if (bobsChoice === 'scissors' && userChoice === 'paper') {
+            return false;
+        } else if (bobsChoice === 'scissors' && userChoice === 'rock') {
+            return true;
+        }
+        
 
 }
 
-main();
+function game (userChoice) {
+    if (bobsScore >= 5 || userScore >= 5) {
+        return;
+    }
 
+    if (userChoice === 'rock') {
 
+        if (determine_winner("rock") == false) {            
+            bobsScore++;
+            bobsScore_span.textContent = bobsScore;
+            resultMessage_p.textContent = "Bob chose paper. Paper beats rock. You lose this round";
+        } else if (determine_winner("rock") === true) {
+            userScore++;
+            userScore_span.textContent = userScore;
+            resultMessage_p.textContent = "Bob chose scissors. Rock beats scissors. You win this round";
+        } else {
+            resultMessage_p.textContent = "Bob also chose rock. You tied this round";
+        }
 
+    } else if (userChoice === 'paper') {
+   
+        if (determine_winner("paper") == false) {
+            bobsScore++;
+            bobsScore_span.textContent = bobsScore;
+            resultMessage_p.textContent = "Bob chose scissors. Scissors beats paper. You lose this round";
+        } else if (determine_winner("paper") === true) {
+            userScore++;
+            userScore_span.textContent = userScore;
+            resultMessage_p.textContent = "Bob chose rock. Paper beats rock. You win this round";
+        } else {
+            resultMessage_p.textContent = "Bob also chose paper. You tied this round";
+        }
+    
+    } if (userChoice === 'scissors') {
+    
+        if (determine_winner("scissors") == false) {
+            bobsScore++;
+            bobsScore_span.textContent = bobsScore;
+            resultMessage_p.textContent = "Bob chose rock. Rock beats scissors. You lose this round";
+        } else if (determine_winner("scissors") === true) {
+            userScore++;
+            userScore_span.textContent = userScore;
+            resultMessage_p.textContent = "Bob chose paper. Scissors beats paper. You win this round";
+        } else {
+            resultMessage_p.textContent = "Bob also chose scissors. You tied this round";
+        }
 
+    }
 
+    if (bobsScore >= 5) {
+        declareWinner_p.textContent = "Game Over. You lose. Try harder next time.";
+        setTimeout(function() {
+            playAgain_button.style.display = "block";
+        }, 1000)
+    } else if (userScore >= 5) {
+        declareWinner_p.textContent = "Game Over. You win. Bob wants a rematch.";
+        setTimeout(function() {
+            playAgain_button.style.display = "block";
+        }, 1000)
+    }
+
+}
+
+function reset_game() {
+
+if (bobsScore >=5 || userScore >=5) {
+    bobsScore = 0;
+    userScore = 0;
+    bobsScore_span.textContent = bobsScore;
+    userScore_span.textContent = userScore;
+    resultMessage_p.textContent = "";
+    declareWinner_p.textContent = "";
+    playAgain_button.style.display = "none";
+
+}}
+
+rock_button.addEventListener('click', function() {game('rock')});
+paper_button.addEventListener('click', function() {game('paper')});
+scissors_button.addEventListener('click', function() {game('scissors')});
+playAgain_button.addEventListener('click', function() {reset_game()});
 
 //      Test randomness of computer choices     //
 
-const bob_rock_span = document.getElementById('bobs-rock-count');
-const bob_paper_span = document.getElementById('bobs-paper-count');
-const bob_scissors_span = document.getElementById('bobs-scissors-count');
+const cpu_rock_span = document.getElementById('cpu-rock-count');
+const cpu_paper_span = document.getElementById('cpu-paper-count');
+const cpu_scissors_span = document.getElementById('cpu-scissors-count');
 
 let rCount = 0;
 let pCount = 0;
@@ -71,14 +143,17 @@ function testRandomness() {
             sCount++;
         }
     }
-    bob_rock_span.textContent = rCount;
-    bob_paper_span.textContent = pCount;
-    bob_scissors_span.textContent = sCount;
+    console.log(rCount);
+    console.log(pCount);
+    console.log(sCount);
 }
 
 document.addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         testRandomness();
+        cpu_rock_span.textContent = "Rock count: " + rCount;
+        cpu_paper_span.textContent = "Paper count: " + pCount;
+        cpu_scissors_span.textContent = "Scissors count: " + sCount;  
     }
 });
 
